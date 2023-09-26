eae5ac5e6d66607d8a1526f003babd231d2ada34
Darly112 <cristhianeduardo112@gmail.com>
2023-09-25 23:55:30 -0600

    se agregar el proyecto

--- src/atoms/ContactInfo.jsx
+++ src/atoms/ContactInfo.jsx
@@ -4,7 +4,7 @@ import { Typography, Box } from '@mui/material';
 
 function ContactInfo() {
   return (
-    <Box sx={{borderRadius:'5px', mb: 2, backgroundColor: 'red', color: 'white', width: '70vh' , p: 2 }}>
+    <Box sx={{borderRadius:'5px', mb: 2, backgroundColor: 'red', color: 'white', width: '80vh' , p: 2 }}>
       <Typography variant="h6" sx={{ color: 'gold' }}>Información de contacto</Typography>
       <Typography variant="body1">Cristhian Rodriguez Hernandez</Typography>
       <Typography variant="body1">NO NAME108@gmail.com</Typography>
--- src/atoms/SocialMedia.jsx
+++ src/atoms/SocialMedia.jsx
@@ -4,7 +4,7 @@ import { Typography, Box } from '@mui/material';
 
 function SocialMedia() {
   return (
-    <Box sx={{borderRadius:'5px', mb: 2, backgroundColor: 'red', color: 'white', width:'62vh' , p: 2, marginLeft: '100px'  }}>
+    <Box sx={{borderRadius:'5px', mb: 2, backgroundColor: 'red', color: 'white', width:'58vh' , p: 2, marginLeft: '100px'  }}>
       <Typography variant="h6" sx={{ color: 'gold' }}>Redes sociales</Typography>
       <Typography variant="body1">ID WZ: NO NAME</Typography>
       <Typography variant="body1">Insta: christian.hdz</Typography>
